0dd3108bd295e65d6100d6f5959f847e71104272
Jakub Hrášek <hrasekj@gmail.com>
2019-04-30 15:51:15 +0200

    Fix missing exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import { Link } from './components/Link';
+import { MobxRouter } from './components/MobxRouter';
 import { generateUrl, routerStateToUrl } from './generate-url';
 import { RouterStore, RouterState, Route } from './router-store';
 import { noopAsync, buildFnsArray, dummyFn } from './utils';
 import { startRouter } from './start-router';
 
 export {
+    MobxRouter, Link,
     generateUrl, routerStateToUrl,
     RouterStore, RouterState, Route,
     startRouter,
