25a44057b057c7c3171eab14e3be291df2a2d927
Jakub Hrášek <hrasekj@gmail.com>
2019-05-01 11:38:16 +0200

    Remove util functions from index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import { Link } from './components/Link';
 import { MobxRouter } from './components/MobxRouter';
 import { generateUrl, routerStateToUrl } from './generate-url';
 import { RouterStore, RouterState, Route } from './router-store';
-import { noopAsync, buildFnsArray, dummyFn } from './utils';
 import { startRouter } from './start-router';
 
 export {
     MobxRouter, Link,
-    generateUrl, routerStateToUrl,
     RouterStore, RouterState, Route,
-    startRouter,
-    noopAsync, buildFnsArray, dummyFn
+    startRouter
 };
