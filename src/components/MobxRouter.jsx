d70f44b326ec685980135920f66586d4284d97a6
Jakub Hrášek <hrasekj@gmail.com>
2019-05-01 11:13:14 +0200

    Remove obsolete console.log

--- src/components/MobxRouter.jsx
+++ src/components/MobxRouter.jsx
@@ -6,7 +6,6 @@ const MobxRouterBase = ({ routerStore, slot }) => {
         console.error('Slot name for MobxRouter must be set!');
     }
     let component = routerStore.currentView[slot];
-    console.log('MobxRouter.render', typeof component);
     return (typeof component === 'function' ? component() : component) || null;
 }
 
